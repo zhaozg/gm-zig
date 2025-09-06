4c31bc853ac433a55231df13eec69a828a061ffc
zhaozg <zhaozg@gmail.com>
2025-09-06 15:14:42 +0800

    new src/testsm9.zig

--- src/testsm9.zig
+++ src/testsm9.zig
@@ -1,11 +1,11 @@
 test {
     // Add SM9 tests - some have infinite loop issues that need to be addressed
-    _ = @import("test/sm9_field_test.zig"); // ✓ Working - 10 tests
+    // _ = @import("test/sm9_field_test.zig"); // ✓ Working - 10 tests
     // TEMPORARILY DISABLE sm9_curve_test while debugging
-    // _ = @import("test/sm9_curve_test.zig"); // ✓ Working - 9 additional tests, but hanging
-
-    // Test if our params.zig fixes resolved the mod test hanging
-    _ = @import("test/sm9_mod_test.zig"); // TEST: Re-enable after params fixes
+    _ = @import("test/sm9_curve_test.zig"); // ✓ Working - 9 additional tests, but hanging
+    //
+    // // Test if our params.zig fixes resolved the mod test hanging
+    // _ = @import("test/sm9_mod_test.zig"); // TEST: Re-enable after params fixes
 
     // Tests with infinite loop issues that need more investigation:
     // These tests hang due to infinite loops in core SM9 algorithms that occur when
@@ -26,8 +26,8 @@ test {
     // _ = @import("test/sm9_random_test.zig");
     // _ = @import("test/sm9_robustness_test.zig");
     // _ = @import("test/sm9_key_agreement_test.zig");
-
-    // Tests that may depend on problematic core functions - test individually:
+    //
+    // // Tests that may depend on problematic core functions - test individually:
     // _ = @import("test/sm9_sign_test.zig");
     // _ = @import("test/sm9_encrypt_test.zig");
     // _ = @import("test/sm9_implementation_test.zig");
